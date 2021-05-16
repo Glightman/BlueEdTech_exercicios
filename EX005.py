2a918ee442db500029e936575690598d8798f091
Gabriel Santana <gabriellima36716@gmail.com>
2021-05-16 15:59:01 -0300

    Update EX005.py

--- EX005.py
+++ EX005.py
@@ -5,6 +5,4 @@ for num in range(0,21):
         tm.sleep(0.5)
         print(num)
 print('\033[04;31mEsses são os numeros ímpares entre 0 e 20!...')
-
-
 print('\033[0;37m_________________________________')
